64d5dfc1dee0d32d943ee97446f936a4f19d8a71
Henrique1818 <luiz.henrique01827@gmail.com>
2023-10-08 23:11:38 -0300

    style: Adicionado link do dropdown do topo; feat: Criado tela de Login e adicionado algumas regras na controller;

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCode.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Autenticar()
+        {
+            string mensagem = ValidarCamposLogin();
+
+            if (string.IsNullOrEmpty(mensagem))
+            {
+                return Redirect("~/");
+            }
+            else
+            {
+                TempData["Erro"] = mensagem;
+                TempData["Email"] = Email;
+                TempData["Senha"] = Senha;
+
+                return Redirect("~/login");
+            }
+        }
+
+        private string ValidarCamposLogin()
+        {
+            string mensagem = string.Empty;
+
+            if (string.IsNullOrEmpty(Email.Trim()))
+            {
+                mensagem = "Por favor, o campo \"E-mail\" é obrigatório.";
+            }
+
+            if (string.IsNullOrEmpty(mensagem))
+            {
+                if (!Email.ToLower().Contains("@"))
+                {
+                    mensagem = "Por favor, o \"E-mail\" informado é inválido.";
+                }
+            }
+
+            if (string.IsNullOrEmpty(mensagem))
+            {
+                if (string.IsNullOrEmpty(Senha.Trim()))
+                {
+                    mensagem = "Por favor, o campo \"Senha\" é obrigatório.";
+                }
+
+                if (string.IsNullOrEmpty(mensagem))
+                {
+                    if (Senha.Length < 8)
+                    {
+                        mensagem = "Por favor, a \"Senha\" dever conter no mínimo 8 caracteres.";
+                    }
+                    
+                    if (string.IsNullOrEmpty(mensagem))
+                    {
+                        if (Senha.Length > 20)
+                        {
+                            mensagem = "Por favor, a \"Senha\" dever conter no máximo 20 caracteres.";
+                        }
+                    }
+                }
+            }
+
+            return mensagem;
+        }
+
+
+        private string Email { get {  return Request.Form["email"].ToString(); } }
+        private string Senha { get { return Request.Form["senha"].ToString(); } }
+    }
+}
