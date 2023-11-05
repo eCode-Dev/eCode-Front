15d6031f24771926c3c96e590c5e693bd5bb7a4b
Henrique1818 <luiz.henrique01827@gmail.com>
2023-11-05 18:48:07 -0300

    Chamando os Desafios da Controle Home do Banco.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,14 @@
-﻿using eCode.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using eCode.Models;
 
 namespace eCode.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return View(new API().ListarDesafiosHome());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
